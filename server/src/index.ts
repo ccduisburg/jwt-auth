d680c8041654fa6ea75a9c747cf0b5df58d8e3f0
ccduisburg <cemilkabakci@gmail.com>
2020-02-22 21:40:40 +0100

    graphql Sercurity

---
+++ server/src/index.ts
@@ -0,0 +1,47 @@
+import "reflect-metadata";
+// import {createConnection} from "typeorm";
+// import {User} from "./entity/User";
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+
+
+(async()=>{
+    const app=express();
+    const apolloServer=new ApolloServer({
+        typeDefs:`
+        type Query {
+            hello:String!
+        }
+        `,
+        resolvers:{
+            Query:{
+                hello:()=>"hello world"
+            }
+        }
+    });
+    apolloServer.applyMiddleware({app});
+
+
+
+    app.listen(4000,()=>{
+        app.get('/',(_req,res)=>res.send("hello"));
+        console.log("express server started");
+    });
+})();
+// createConnection().then(async connection => {
+
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
+
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
+
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
